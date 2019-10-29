639f413d3b419b7fffdd8388c1e45d9c5914aeaa
Alex Salvetti <7389032+alexsalvetti@users.noreply.github.com>
2019-10-29 13:50:19 +0100

    Update listen-display.sh

--- listen-display.sh
+++ listen-display.sh
@@ -13,7 +13,7 @@
 # Carry out specific functions when asked to by the system
 case "$1" in
   start)
-    echo "Starting listen.py"
+    echo "Starting listen-display.py"
     /usr/local/bin/listen-display.py &
     ;;
   stop)
